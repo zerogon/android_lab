26e87f8ac615cf3446a81e243a8526a6d74a7327
zerogon <zerogon@lotte.net>
2020-01-28 18:04:29 +0900

    first commit22

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var txt = findViewById<TextView>(R.id.txtHello)
+        txt.text = "안녕하세요"
+        txt.textSize = 32.0F
+        txt.setTextColor(Color.parseColor("#FF0000"))
     }
 }
