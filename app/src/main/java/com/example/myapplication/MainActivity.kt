ad837d1a72d25c58f55b1c279f868245afef294d
Renovo <safoma@naver.com>
2020-02-02 21:09:05 +0900

    logcat 사용하기

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -16,10 +16,46 @@ class MainActivity : AppCompatActivity() {
             Log.d(javaClass.name,"${nCount++} clicked")
             try{
                 val nResult = nMaxCount /(nMaxCount-nCount)
-                Log.d("MyLog", "nMaxCount/(nMaxCount-nCount) is ${nResult}")
+                Log.d("MyLog", "nMaxCount/(nMaxCount-nCount) is ${nResult} ")
             }catch (e:Exception){
                 Log.d("MyLog","${nCount}: ${e.toString()}" )
             }
         }
     }
+    /*
+    *     var nLineNumber = 0
+
+    // Activity가 생성될 때
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("ALLTEST", String.format("\n%d: onCreate", nLineNumber++))
+    }
+
+    // 내 activity 맨앞으로 왔을 때
+    public override fun onResume() {
+        super.onResume()
+        Log.d("ALLTEST", String.format("%d: onResume", nLineNumber++))
+    }
+
+    // 내 activity 맨앞에 뭔가가 있을 때
+    public override fun onStop() {
+        super.onStop()
+        Log.d("ALLTEST", String.format("%d: onStop", nLineNumber++))
+    }
+
+    // Activity가 최초실행될 때
+    public override fun onStart() {
+        super.onStart()
+        Log.d("ALLTEST", String.format("%d: onStart", nLineNumber++))
+    }
+
+    // Activity가 완전히 종료될 때
+    public override fun onDestroy() {
+        super.onDestroy()
+        Log.d("ALLTEST", String.format("%d: onDestroy", nLineNumber++))
+    }
+    *
+    * */
 }
